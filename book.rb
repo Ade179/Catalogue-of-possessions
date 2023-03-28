a4742513cd8884b9b534e81b262e2856cce6a99d
Juwon <dnladesanya1@gmail.com>
2023-03-28 10:44:30 +0100

    fixed linters error

--- book.rb
+++ book.rb
@@ -5,7 +5,7 @@ class Book < Item
   attr_accessor :label, :genre, :author, :publisher, :cover_state
 
   def initialize(publisher, cover_state, publish_date, archived)
-    super(publish_date:publish_date, archived:archived)
+    super(publish_date: publish_date, archived: archived)
     @publisher = publisher
     @cover_state = cover_state
   end
