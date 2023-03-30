a42a6662c4e5ed7ac0439c9cddddbe9563e0ca63
Juwon <dnladesanya1@gmail.com>
2023-03-30 11:07:32 +0100

    tests for newly implemented methods in book and label class

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,21 +1,56 @@
-require_relative '../label'
+require 'date'
 require_relative '../item'
+require_relative '../book'
 
-describe Label do
-  context 'When providing Label information' do
-    label = Label.new('Women of Owu', 'Blue')
+RSpec.describe Book do
+  let(:publisher) { 'william-shakespeare' }
+  let(:cover_state) { 'bad' }
+  let(:publish_date) { '2021-03-07' }
+  let(:archived) { true }
 
-    it 'creates a new label with title Women of Owu' do
-      expect(label).to be_an_instance_of(Label)
+  subject { described_class.new(publisher, cover_state, publish_date, archived) }
+
+  describe '#can_be_archived?' do
+    context 'when the book is more than 10 years old' do
+      let(:publish_date) { "#{Time.new.year - 11}-03-07" }
+
+      it 'returns true' do
+        expect(subject.can_be_archived?).to eq(true)
+      end
+    end
+
+    context 'when the book is less than 10 years old' do
+      let(:publish_date) { "#{Time.new.year - 9}-03-07" }
+
+      it 'returns false' do
+        expect(subject.can_be_archived?).to eq(true)
+      end
+    end
+
+    context 'when the book cover state is bad' do
+      let(:cover_state) { 'bad' }
+
+      it 'returns true' do
+        expect(subject.can_be_archived?).to eq(true)
+      end
+    end
+
+    context 'when the book cover state is good' do
+      let(:cover_state) { 'good' }
+
+      it 'returns false' do
+        expect(subject.can_be_archived?).to eq(false)
+      end
     end
   end
 
-  context 'when adding a new book' do
-    label = Label.new('Tempest', 'red')
-    it 'adds a new book to the label' do
-      item = Item.new(publish_date: '2021-03-03', archived: true)
-      label.add_item(item)
-      expect(label.items[0]).to eql item
+  describe '#to_s' do
+    it 'returns a string representation of the book' do
+      y = "<#{described_class} ID: #{subject.id}>" \
+          "[Publisher: #{publisher}, Cover_state: #{cover_state}, publish_date: #{publish_date}, archive: #{archived}]"
+
+
+      expect(subject.to_s).to eq(y)
     end
   end
 end
