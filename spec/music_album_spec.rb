b1bb7c33835a7875886812707ad8a7e303f2288f
peter <peterugwu64@gmail.com>
2023-03-30 16:39:03 +0100

    Fix linters

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -8,35 +8,35 @@ describe MusicAlbum do
   end
 
   describe '#to_s' do
-  it 'returns a string of the music_album' do
-    music_album = MusicAlbum.new('Dusk till Down', 'Alan Walker', '2016-08-01', true)
-    expout = "<MusicAlbum ID: #{music_album.id}>[name: Dusk till Down, artist: Alan Walker, publish_date: 2016-08-01, on_spotify: true]"
-
-    expect(music_album.to_s).to eq(expout)
-  end
-end
-
-describe '#to_h' do
-  it 'returns a hash of the music album' do
-    music_album = MusicAlbum.new('Cool you are', 'DBlues', '2016-02-01', false)
-    expout = {
-      id: music_album.id,
-      artist: 'DBlues',
-      name: 'Cool you are',
-      publish_date: '2016-02-01',
-      on_spotify: false
-    }
-    expect(music_album.to_h).to eq(expout)
-  end
-end
-
-    describe '#can_be_archived?' do
-      it 'exists' do
-        expect(@music).to respond_to(:can_be_archived?)
-      end
-
-      it 'returns false' do
-        expect(@music.can_be_archived?).to be_falsey
-      end
+    it 'returns a string of the music_album' do
+      music_album = MusicAlbum.new('Dusk till Down', 'Alan Walker', '2016-08-01', true)
+      expout = "<MusicAlbum ID: #{music_album.id}>[name: Dusk till Down,"  \
+               ' artist: Alan Walker, publish_date: 2016-08-01, on_spotify: true]'
+      expect(music_album.to_s).to eq(expout)
     end
   end
+
+  describe '#to_h' do
+    it 'returns a hash of the music album' do
+      music_album = MusicAlbum.new('Cool you are', 'DBlues', '2016-02-01', false)
+      expout = {
+        id: music_album.id,
+        artist: 'DBlues',
+        name: 'Cool you are',
+        publish_date: '2016-02-01',
+        on_spotify: false
+      }
+      expect(music_album.to_h).to eq(expout)
+    end
+  end
+
+  describe '#can_be_archived?' do
+    it 'exists' do
+      expect(@music).to respond_to(:can_be_archived?)
+    end
+
+    it 'returns false' do
+      expect(@music.can_be_archived?).to be_falsey
+    end
+  end
+end
